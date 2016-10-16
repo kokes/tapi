75e0a97025fd6e0391b8377cc5b9567932bf83ea
Ondrej Kokes <ondrej.kokes@gmail.com>
2016-10-16 14:06:00 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+Playing with the http stack in golang, mostly an educational thing
+
+`go run tapi.go` and open the browser to localhost:7678
+
+
+fix
+
+- open browser
+- embed HTML in Go code
+- proper httptrace timing
+- handle redirs
+- send errors in JSON
+- syntax highlight JSON?
+- pretty print HTML
+- tests using httptest
+- gzip compression
+- treat timeout properly
+- [atob decoding utf8 problem](http://stackoverflow.com/questions/30106476/using-javascripts-atob-to-decode-base64-doesnt-properly-decode-utf-8-strings)
---
+++ tapi.go
@@ -0,0 +1,122 @@
+package main
+
+import (
+	"bytes"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
+	"time"
+)
+
+type RData struct {
+	StatusCode int               `json:"status_code"`
+	Protocol   string            `json:"protocol"`
+	Headers    map[string]string `json:"headers"`
+	Body       string            `json:"body"`
+	Timing     int64             `json:"timing"`
+}
+
+func main() {
+	fmt.Println("Serving at http://localhost:7678")
+	http.HandleFunc("/", route)
+	log.Fatal(http.ListenAndServe(":7678", nil))
+}
+
+func route(w http.ResponseWriter, r *http.Request) {
+	pth := strings.TrimRight(r.URL.Path, "/")
+
+	switch pth {
+	case "":
+		// fmt.Fprintf(w, "index this, biatch")
+		w.Header().Set("Content-Type", "text/html")
+
+		dt, _ := ioutil.ReadFile("index.html")
+		w.Write(dt)
+
+	case "/ping":
+		fmt.Fprintf(w, "pong")
+	case "/query":
+		dt, err := handleRequest(w, r)
+		if err != nil {
+			panic(err) // TODO: return JSON with the error instead
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(dt)
+
+	default:
+		fmt.Fprintf(w, "ne hablo")
+
+	}
+}
+
+func handleRequest(w http.ResponseWriter, r *http.Request) ([]byte, error) {
+	// qr := r.URL.Query()
+
+	method := strings.ToUpper(r.PostFormValue("method"))
+	url := r.PostFormValue("url")
+	log.Print(r.ParseForm())
+	log.Print(r.Form)
+
+	rd, err := makeRequest(method, url, nil)
+	if err != nil {
+		return []byte{}, err
+	}
+
+	b, err := json.Marshal(rd)
+	if err != nil {
+		return []byte{}, err
+	}
+
+	var out bytes.Buffer
+	json.Indent(&out, b, "", "\t")
+	// ioutil.WriteFile("res-b64.txt", []byte(rd.Body), 0666)
+
+	return b, nil
+
+}
+
+func makeRequest(method, url string, bd io.Reader) (RData, error) {
+	client := &http.Client{} // TODO: handle redirects?
+
+	req, err := http.NewRequest(method, url, nil)
+	if err != nil {
+		return RData{}, err
+	}
+
+	t := time.Now()
+	resp, err := client.Do(req)
+	tm := time.Since(t)
+	if err != nil {
+		return RData{}, err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	// ioutil.WriteFile("res.txt", body, 0666)
+	if err != nil {
+		return RData{}, err
+	}
+	b64body := base64.StdEncoding.EncodeToString(body)
+
+	// the header is map[string][]string, we need to flatten it
+	hd := make(map[string]string)
+	for k, v := range resp.Header {
+		hd[k] = strings.Join(v, "; ")
+	}
+
+	rd := RData{
+		StatusCode: resp.StatusCode,
+		Protocol:   resp.Proto,
+		Headers:    hd,
+		Body:       b64body,
+		Timing:     int64(tm) / (1000 * 1000),
+	}
+
+	return rd, nil
+}
